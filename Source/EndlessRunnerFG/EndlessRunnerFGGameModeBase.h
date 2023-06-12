0811c7d09863ced465fab59662fcdd098efb7b74
PapperoniHD <gabrielling@hotmail.se>
2023-06-12 21:45:57 +0200

    Initial push

---
+++ Source/EndlessRunnerFG/EndlessRunnerFGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndlessRunnerFGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDLESSRUNNERFG_API AEndlessRunnerFGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
